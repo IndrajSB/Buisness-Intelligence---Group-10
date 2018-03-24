8bb868bcdb4635c3ce2706883b3a06c2c549eb70
p16165888 <p16165888@KLIXX201WIN002W>
2018-03-24 17:54:12 +0000

    Customer Changes

--- GITTest/Customer.Designer.cs
+++ GITTest/Customer.Designer.cs
@@ -28,11 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCustomer = new System.Windows.Forms.Button();
+            this.lstCustomer = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCustomer
+            // 
+            this.btnCustomer.Location = new System.Drawing.Point(350, 176);
+            this.btnCustomer.Name = "btnCustomer";
+            this.btnCustomer.Size = new System.Drawing.Size(99, 45);
+            this.btnCustomer.TabIndex = 0;
+            this.btnCustomer.Text = "Get Customer";
+            this.btnCustomer.UseVisualStyleBackColor = true;
+            this.btnCustomer.Click += new System.EventHandler(this.btnCustomer_Click);
+            // 
+            // lstCustomer
+            // 
+            this.lstCustomer.FormattingEnabled = true;
+            this.lstCustomer.Location = new System.Drawing.Point(46, 74);
+            this.lstCustomer.Name = "lstCustomer";
+            this.lstCustomer.Size = new System.Drawing.Size(288, 147);
+            this.lstCustomer.TabIndex = 1;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 380);
+            this.Controls.Add(this.lstCustomer);
+            this.Controls.Add(this.btnCustomer);
+            this.Name = "Customer";
             this.Text = "Customer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCustomer;
+        private System.Windows.Forms.ListBox lstCustomer;
     }
 }
--- GITTest/Customer.cs
+++ GITTest/Customer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,47 @@ namespace GITTest
         public Customer()
         {
             InitializeComponent();
+        }
+
+        private void btnCustomer_Click(object sender, EventArgs e)
+        {
+            List<string> Customer = new List<string>();
+            //clear the listbox
+            lstCustomer.Items.Clear();
+
+            //create the database string
+            string connectionString = Properties.Settings.Default.Data_set_1ConnectionString;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand Customers = new OleDbCommand("SELECT [Customer ID], [Customer Name] from Sheet1'", connection);
+
+                reader = Customers.ExecuteReader();
+                while (reader.Read())
+                {
+                    Customer.Add(reader[0].ToString());
+                    Customer.Add(reader[1].ToString());
+
+                }
+            }
+            //create a new list for the formatted data
+            List<string> CustomerFormatted = new List<string>();
+
+            foreach (string customers in Customer)
+            {
+                //split the string on whitespace and remove anything thats blank
+                var customer = customers.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                //grab the first item (we know this is the date) and add it to our new list
+                CustomerFormatted.Add(customer[0]);
+            }
+
+            //Blind the listbox to the list
+            lstCustomer.DataSource = CustomerFormatted;
+
+            //listBoxDates.DataSource = Dates;
+
         }
     }
 }
