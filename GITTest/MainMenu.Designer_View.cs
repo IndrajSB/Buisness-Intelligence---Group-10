a6cdc0071bc7a0a726a0e5665688633aa32e5073
Duleep <p1617260x@my365.dmu.ac.uk>
2018-04-15 18:08:52 +0100

    File naming convention improved and fitted to learning objectives

---
+++ GITTest/MainMenu.Designer_View.cs
@@ -0,0 +1,100 @@
+﻿namespace GITTest
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnDates = new System.Windows.Forms.Button();
+            this.btnProduct = new System.Windows.Forms.Button();
+            this.lblSystem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Location = new System.Drawing.Point(100, 130);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Size = new System.Drawing.Size(75, 23);
+            this.btnCustomers.TabIndex = 0;
+            this.btnCustomers.Text = "Customers";
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_ClickEvent);
+            // 
+            // btnDates
+            // 
+            this.btnDates.Location = new System.Drawing.Point(100, 159);
+            this.btnDates.Name = "btnDates";
+            this.btnDates.Size = new System.Drawing.Size(75, 23);
+            this.btnDates.TabIndex = 1;
+            this.btnDates.Text = "Dates";
+            this.btnDates.UseVisualStyleBackColor = true;
+            this.btnDates.Click += new System.EventHandler(this.btnDates_ClickEvent);
+            // 
+            // btnProduct
+            // 
+            this.btnProduct.Location = new System.Drawing.Point(100, 188);
+            this.btnProduct.Name = "btnProduct";
+            this.btnProduct.Size = new System.Drawing.Size(75, 23);
+            this.btnProduct.TabIndex = 2;
+            this.btnProduct.Text = "Product";
+            this.btnProduct.UseVisualStyleBackColor = true;
+            this.btnProduct.Click += new System.EventHandler(this.btnProduct_ClickEvent);
+            // 
+            // lblSystem
+            // 
+            this.lblSystem.AutoSize = true;
+            this.lblSystem.Font = new System.Drawing.Font("Sitka Subheading", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSystem.Location = new System.Drawing.Point(95, 9);
+            this.lblSystem.Name = "lblSystem";
+            this.lblSystem.Size = new System.Drawing.Size(79, 30);
+            this.lblSystem.TabIndex = 3;
+            this.lblSystem.Text = "System";
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSystem);
+            this.Controls.Add(this.btnProduct);
+            this.Controls.Add(this.btnDates);
+            this.Controls.Add(this.btnCustomers);
+            this.Name = "MainMenu";
+            this.Text = "MainMenu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnDates;
+        private System.Windows.Forms.Button btnProduct;
+        private System.Windows.Forms.Label lblSystem;
+    }
+}
