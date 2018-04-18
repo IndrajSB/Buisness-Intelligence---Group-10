49064619aa1aa26f7eb6ff64edb402aa2a919931
p16165888 <p16165888@KLIXX201WIN008W>
2018-04-18 03:11:48 +0100

    Customer

--- GITTest/Customer.cs
+++ GITTest/Customer.cs
@@ -66,7 +66,7 @@ namespace GITTest
 
         private void btnGetCustomer_Click(object sender, EventArgs e)
         {
-            //create new list to store the result in
+              //create new list to store the result in
             List<string> Customers = new List<string>();
             //clear the list box
             lstCustomer.Items.Clear();
